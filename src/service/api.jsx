78c0aecbc99ec4d4c6a32f10b08b9ff2f3ceaa3c
luizlimadev <devluizlima@gmail.com>
2023-05-19 10:03:48 -0300

    correcao endereco API

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://json-server-favoriteflix.vercel.app//',
+  baseURL: 'https://json-server-favoriteflix.vercel.app',
   headers: { 'Content-Type': 'Application/json' }
 });
 
